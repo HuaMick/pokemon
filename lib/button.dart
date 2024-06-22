d7e1a2a7e46a8cfe6e99222f1ec9171cd522c566
HuaMick <Hua.Mick@Gmail.com>
2024-06-22 20:47:08 +1000

    init

---
+++ lib/button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final text;
+  final function;
+
+  MyButton({this.text, this.function});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: function,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(
+              color: Colors.white,
+              width: 2,
+            ),
+          ),
+          height: 50,
+          width: 50,
+          child: Center(
+            child:Text(
+            text,
+            style: TextStyle(color: Colors.white,fontSize: 20),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,203 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:pokemon/button.dart';
+import 'package:pokemon/characters/boy.dart';
+import 'package:pokemon/maps/littleroot.dart';
+import 'package:pokemon/characters/boy.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  // littleroot
+  double mapX = 0;
+  double mapY = 0;
+
+  // game stuff
+  String currentLocation = 'littleroot';
+
+  void moveDown(){
+    setState(() {
+      mapY -= 0.2;
+    });
+  }
+  void moveUp(){
+    setState(() {
+      mapY += 0.2;
+    });
+  }
+  void moveLeft(){
+    setState(() {
+      mapX += 0.2;
+    });
+  }
+  void moveRight(){
+    setState(() {
+      mapX -= 0.2;
+    });
+  }
+  void pressedA(){
+    print('A');
+  }
+  void pressedB(){
+    print('B');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          AspectRatio(
+            aspectRatio: 1,
+            child: Container(
+              color: Colors.black,
+              child: Stack(
+                children: [
+                  // littleroot
+                  LittleRoot(
+                    x: mapX, 
+                    y: mapY, 
+                    currentMap: currentLocation),
+                
+                 // character boy
+                 Container(
+                  alignment: Alignment(0,0),
+                  child: MyBoy(),
+                 )
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.grey[900],
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'G A M E B O Y', 
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Row(
+                          children: [
+                            Row(
+                              children: [
+                                Column(
+                                  children: [
+                                    Container(
+                                      height: 50,
+                                      width: 50,
+                                    ),
+                                    MyButton(
+                                      text: '←',
+                                      function: moveLeft,
+                                    ),
+                                    Container(
+                                      height: 50,
+                                      width: 50,
+                                    ),                                                                        
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    MyButton(
+                                      text: '↑',
+                                      function: moveUp,
+                                    ),
+                                    Container(
+                                      height: 50,
+                                      width: 50,
+                                    ),
+                                    MyButton(
+                                      text: '↓',
+                                      function: moveDown,
+                                    ),                                                                        
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    Container(
+                                      height: 50,
+                                      width: 50,
+                                    ),
+                                    MyButton(
+                                      text: '→',
+                                      function: moveRight,
+                                    ),
+                                    Container(
+                                      height: 50,
+                                      width: 50,
+                                    ),                                                                        
+                                  ],
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Column(
+                              children: [
+                                Container(
+                                  height: 50,
+                                  width: 50,
+                                ),
+                                MyButton(
+                                  text: 'a',
+                                  function: pressedA,
+                                ),
+                              ]
+                            ),
+                            Column(
+                              children: [
+                                MyButton(
+                                  text: 'b',
+                                  function: pressedB,
+                                ),
+                                Container(
+                                  height: 50,
+                                  width: 50,
+                                ),
+                              ]
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Text(
+                      'C R E A T E D B Y R I C E B O Y',
+                      style: TextStyle(color: Colors.white)
+                      ),
+                  ],
+                )
+              ),
+            ),
+          ),
+      ],)
+    );
+  }
+}
